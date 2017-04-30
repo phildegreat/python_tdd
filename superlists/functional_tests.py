50676eba1bea4e1788a20ff16ba0685da6c0cc02
Phil Chan <pc@Avalon-MkIV.local>
2017-04-30 14:16:47 +1000

    Third commit: make use of UnitTest library, rewrite test with test class

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,11 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# check home page
+		self.browser.get('http://localhost:8000')
+
+		# Check page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+"""# Invited to enter a to-do item straight away
+		[...rest of comments as before]
+"""
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
+"""browser = webdriver.Chrome()
 
 # Check home page availability
 browser.get('http://localhost:8000')
 
 # Notice the title and header
-assert 'To-Do' in browser.title
+assert 'To-Do' in browser.title, "Browser title was " + browser.titlee
 
 browser.quit()
+"""
