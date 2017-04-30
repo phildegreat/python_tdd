cc66de3d3432da0c944c0279322a910f0943c1c9
Phil Chan <pc@Avalon-MkIV.local>
2017-04-30 12:44:49 +1000

    Modify test file

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+# Check home page availability
 browser.get('http://localhost:8000')
 
+# Notice the title and header
 assert 'Django' in browser.title
+
+browser.quit()
